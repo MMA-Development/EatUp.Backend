c0a348efc2a7e3a78781a26b9c3ab6151752fa9b
Mikkel Gundersen <mig@netly.dk>
2025-05-22 09:25:25 +0200

    Add Hangfire dashboard authorization and configuration
    - Introduced `Hangfire.Dashboard.BasicAuthorization` package for basic auth.
    - Updated `Program.cs` to configure dashboard options and replace job registration.
    - Added `Hangfire` credentials in `appsettings.json`.
    - Modified `docker-compose.yaml` for image tag and environment variables.
    - Exposed Hangfire dashboard on port `8081`.

--- EatUp.Hangfire/Program.cs
+++ EatUp.Hangfire/Program.cs
@@ -1,6 +1,8 @@
 using EatUp.Hangfire;
 using EatUp.Hangfire.Jobs;
 using Hangfire;
+using Hangfire.Dashboard;
+using Hangfire.Dashboard.BasicAuthorization;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +17,28 @@ builder.Services.AddTransient<IRabbitMqPublisher>(x =>
 
 
 builder.Services.AddScoped<PerformVendorHardResyncJob>();
+builder.Services.AddScoped<PerformUserHardResyncJob>();
+builder.Services.AddScoped<PerformMealHardResyncJob>();
 
+var options = new DashboardOptions
+{
+    Authorization = new[] { new BasicAuthAuthorizationFilter(
+        new BasicAuthAuthorizationFilterOptions
+        {
+            RequireSsl = false, // true in production
+            SslRedirect = false,
+            LoginCaseSensitive = true,
+            Users = new[]
+            {
+                new BasicAuthAuthorizationUser
+                {
+                    Login = builder.Configuration["Hangfire:Username"] ,
+                    PasswordClear = builder.Configuration["Hangfire:Password"]
+                }
+            }
+        })
+    }
+};
 
 // Add services to the container.
 builder.Services.AddHangfire(configuration => configuration.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
@@ -29,7 +52,7 @@ var app = builder.Build();
 
 EnsureDatabaseCreated.Ensure(builder.Configuration.GetConnectionString("HangfireConnection"));
 
-app.UseHangfireDashboard();
+app.UseHangfireDashboard(options: options);
 
 ConfigureJobs.Configure(app);
 
