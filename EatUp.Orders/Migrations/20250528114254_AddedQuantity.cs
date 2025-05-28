7c1cee375fa7e58f338e7856e8416a78c89ba4fe
Mikkel Gundersen <mig@netly.dk>
2025-05-28 13:43:03 +0200

    Added quantity

---
+++ EatUp.Orders/Migrations/20250528114254_AddedQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EatUp.Orders.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "MealProjections",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "MealProjections");
+        }
+    }
+}
